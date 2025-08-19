2ccbaa7f43835c9f6bb929b9dcc32f73fb0e7fc4
Javier <javier.merino.maldonado@gmail.com>
2025-08-19 13:54:35 -0700

    portfolio right

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import { BrowserRouter as Router } from "react-router-dom";
 import HeroSection from "./components/HeroSection";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
-import Contact from "./components/Contact";
 import Pictures from "./components/Pictures";
 import Footer from "./components/Footer";
 import Experience from "./components/Experience";
@@ -38,7 +37,6 @@ function App() {
           <div>
             <Pictures />
             <Education />
-            <Contact />
           </div>
           <Footer />
           {openModal.state && (
